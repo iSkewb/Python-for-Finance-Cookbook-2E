188deb08fb0110df83da8b5b8c04f080493c722f
iSkewb <dbmeyer05@outlook.com>
2023-12-12 22:55:06 -0700

    update rtns

--- 01_acquiring_financial_data/SimpleLogReturns.py
+++ 01_acquiring_financial_data/SimpleLogReturns.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # download data and keep adjusted close prices only
 df = yf.download("AAPL",
-                 start="2011-01-01",
-                 end="2021-12-31",
+                 start="2010-01-01",
+                 end="2020-12-31",
                  progress=False)
 df = df.loc[:, ["Adj Close"]]
 
@@ -14,4 +14,4 @@ df["simple_rtn"] = df["Adj Close"].pct_change()
 df["log_rtn"] = np.log(df["Adj Close"]/df["Adj Close"].shift(1))
 
 # inspect output
-df.head(10)
+df.head()
